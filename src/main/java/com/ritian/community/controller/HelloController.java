edda8070024ffc7b7e56d391180d64c2d63bd97a
wth17854117835 <17854117835@163.com>
2019-10-26 09:12:10 +0800

    Initial commit

---
+++ src/main/java/com/ritian/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.ritian.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author: wangth_oup
+ * @date: 2019-10-26 8:45
+ * @description:
+ **/
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+}
